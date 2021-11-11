9c370a43ad78c194ef975b469076e995d741c83e
TAIEBTRABELSI <taiebtrabelsi4@gmail.com>
2021-11-11 18:12:54 +0000

    checkpointcomps

---
+++ src/Componant/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+           <h2>taiebtrabelsi4@gamil.com</h2> 
+        </div>
+    )
+}
+
+export default Address;
---
+++ src/Componant/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Img3 from '../../Img3.jpg'
+const ProfilePhoto = () => {
+    return (
+        <div>
+        <img src= {Img3} alt=""   style={{width: 400, height: 400, borderRadius: 400/ 2}}  /> 
+
+        </div>
+    )
+}
+
+export default ProfilePhoto;
